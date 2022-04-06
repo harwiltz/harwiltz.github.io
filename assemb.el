8d350c04f5cb2a1bb247d9bf8fe73cc1d0813031
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2022-04-05 20:18:01 -0400

    Separates publish script from assemb.el

--- assemb.el
+++ assemb.el
@@ -3,24 +3,7 @@
 
 (setq assemble-outdir "build")
 
+(load-file "./publish.el")
+
 (defun default()
-  (let ((org-confirm-babel-evaluate nil)
-	(org-project-root default-directory)
-	(image-file-name-extensions (cons "webm" image-file-name-extensions))
-	(org-publish-project-alist
-	 `(("org-notes"
-	    :base-directory "."
-	    :base-extension "org"
-	    :publishing-directory ,assemble-outdir
-	    :recursive t
-	    :publishing-function org-html-publish-to-html
-	    :headline-levels 4
-	    :auto-preamble t)
-	   ("org-static"
-	    :base-directory "./assets"
-	    :base-extension "css\\|js\\|jpg\\|png\\|pdf\\|gif\\|webm"
-	    :publishing-directory ,(concat assemble-outdir "/assets")
-	    :recursive t
-	    :publishing-function org-publish-attachment)
-	   ("org" :components ("org-notes" "org-static")))))
-	(org-publish "org")))
+  (publish-site assemble-outdir))
---
+++ publish.el
@@ -0,0 +1,23 @@
+(require 'ox-publish)
+
+(defun publish-site(dir &optional force)
+  (let ((org-confirm-babel-evaluate nil)
+	(org-project-root default-directory)
+	(image-file-name-extensions (cons "webm" image-file-name-extensions))
+	(org-publish-project-alist
+	 `(("org-notes"
+	    :base-directory "."
+	    :base-extension "org"
+	    :publishing-directory ,dir
+	    :recursive t
+	    :publishing-function org-html-publish-to-html
+	    :headline-levels 4
+	    :auto-preamble t)
+	   ("org-static"
+	    :base-directory "./assets"
+	    :base-extension "css\\|js\\|jpg\\|png\\|pdf\\|gif\\|webm"
+	    :publishing-directory ,(concat dir "/assets")
+	    :recursive t
+	    :publishing-function org-publish-attachment)
+	   ("org" :components ("org-notes" "org-static")))))
+    (org-publish "org" force)))
